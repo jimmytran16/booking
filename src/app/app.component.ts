b6edcbe13a4716e0f0d08342b64adb4cff2f05fe
jimmytran16 <jimmytran1620@gmail.com>
2024-07-20 18:41:33 -0400

    finish date pick and available times, stlying

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,27 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = 'dns-scheduling-app';
-  readonly bestBoys: string[] = ['Samoyed', 'Akita Inu', 'Alaskan Malamute', 'Siberian Husky'];
+  readonly availableTimes: string[] = this.generateTimeAvailibilities();
+
+
+  // Generate an array of available times for the day
+  // NOTE: May need to fetch from DB that way we can munipulate the times if need be
+  private generateTimeAvailibilities(): string[] {
+    var availibleTimes: string[] = []
+    var availibleHours = [9,10,11,12,1,2,3,4,5,6,7];
+    var availableMinutes = [0, 15, 30, 45]
+    availibleHours.forEach(h => {
+      availableMinutes.forEach(m => {
+        availibleTimes.push(`${h}:${m == 0 ? "00": m} ${this.getAMOrPM(h)}`)
+      })
+    })
+
+    return availibleTimes;
+  }
+
+  private getAMOrPM(time: number): string {
+    return time >= 9 && time < 12 
+      ? "AM"
+      : "PM"
+  }
 }
